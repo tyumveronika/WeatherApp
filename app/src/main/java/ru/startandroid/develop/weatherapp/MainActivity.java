9b4f1ee11fcba2a1273eca41e1f4d76030a3c1e0
tyumveronika <veronikatyumentseva@gmail.com>
2023-04-26 13:01:43 +0900

    hthtt

---
+++ app/src/main/java/ru/startandroid/develop/weatherapp/MainActivity.java
@@ -0,0 +1,161 @@
+package ru.startandroid.develop.weatherapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.project.core.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    TextView view_city;
+    TextView view_temp;
+    TextView view_desc;
+
+    ImageView view_weather;
+    EditText search;
+    FloatingActionButton search_floating;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view_city = findViewById(R.id.town);
+        view_temp = findViewById(R.id.temp);
+        view_desc = findViewById(R.id.desc);
+        view_city.setText("");
+        view_temp.setText("");
+        view_desc.setText("");
+
+        view_weather = findViewById(R.id.weatherImage);
+        search = findViewById(R.id.editText);
+        search_floating = findViewById(R.id.floating_search);
+
+        search_floating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager imn = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imn.hideSoftInputFromWindow(getCurrentFocus().getRootView().getWindowToken(), 0);
+                api_key(String.valueOf(search.getText()));
+            }
+        });
+    }
+
+    private void api_key(final String City) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?q=" + search.getText().toString() + "&appid=3a0af78e48c6ead1692f71a067fea318&units=metric")
+                .get()
+                .build();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            Response response = client.newCall(request).execute();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    String responseData = response.body().string();
+                    try {
+                        JSONObject json = new JSONObject(responseData);
+                        JSONArray array = json.getJSONArray("weather");
+                        JSONObject object = array.getJSONObject(0);
+
+                        String description = object.getString("description");
+                        String icons = object.getString("icon");
+
+                        JSONObject temp1 = json.getJSONObject("main");
+                        Double Temperature = temp1.getDouble("temp");
+
+                        setText(view_city, City);
+                        String temps = Math.round(Temperature) + " °C";
+                        setText(view_temp, temps);
+                        setText(view_desc, description);
+                        setImage(view_weather, icons);
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setText(final TextView text, final String value) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.setText(value);
+            }
+        });
+    }
+
+    private void setImage(final ImageView imageView, final String value) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                switch (value) {
+                    case "01d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon1));
+                        break;
+                    case "01n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon1));
+                        break;
+                    case "02d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon2));
+                        break;
+                    case "02n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon2));
+                        break;
+                    case "03d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon3));
+                        break;
+                    case "03n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon3));
+                        break;
+                    case "04d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon4));
+                        break;
+                    case "04n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon4));
+                        break;
+                    case "09d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon5));
+                        break;
+                    case "09n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon5));
+                        break;
+                    case "10d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon6));
+                        break;
+                    case "10n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon6));
+                        break;
+                    case "11d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon7));
+                        break;
+                    case "11n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon7));
+                        break;
+
+                    default:
+                        imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather));
+                }
+            }
+        });
+    }
+}
